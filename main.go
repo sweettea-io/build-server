40cfb68b875b706995c27aae6df7160174a341ba
Ben Whittle <benwhittle31@gmail.com>
2018-07-19 13:18:07 -0700

    outline main process

--- main.go
+++ main.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import (
+  "github.com/sweettea-io/build-server/internal/app"
+  "github.com/sweettea-io/build-server/internal/pkg/config"
+)
+
+func main() {
+  // Initialize the app.
+  app.Init(config.New())
+
+  // Git clone build target.
+
+  // Validate build target's config file.
+
+  // Git clone buildpack.
+
+  // Attach buildpack to target.
+
+  // Build augmented target into Docker image.
+
+  // Push Docker image to external repository.
+}
