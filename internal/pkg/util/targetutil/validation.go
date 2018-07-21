85712c666d84a435efdec78212c66955b986cbb9
Ben Whittle <benwhittle31@gmail.com>
2018-07-21 08:51:56 -0700

    add yaml dep; validate target config file exists & unmarshal it into struct

--- internal/pkg/util/targetutil/validation.go
+++ internal/pkg/util/targetutil/validation.go
@@ -1,7 +1,50 @@
 package targetutil
 
+import (
+  "errors"
+  "fmt"
+  "io/ioutil"
+  "strings"
+  "github.com/sweettea-io/build-server/internal/pkg/util/fileutil"
+  "gopkg.in/yaml.v2"
+)
+
 const ConfigFile = ".sweettea.yml"
 
-func ValidateConfig(path string) error {
+type Config struct {
+  Key string
+}
+
+func ValidateConfig(dir string) error {
+  // Build path to config file from provided directory.
+  configPath := fmt.Sprintf("%s/%s", strings.TrimRight(dir, "/"), ConfigFile)
+
+  // Ensure config file exists.
+  configExists, err := fileutil.Exists(configPath)
+
+  if err != nil {
+    return err
+  }
+
+  if !configExists {
+    return errors.New("target config file not found")
+  }
+
+  // Read config file into byte array.
+  configData, err := ioutil.ReadFile(configPath)
+
+  if err != nil {
+    return err
+  }
+
+  // Unmarshal config file into struct.
+  var config Config
+
+  if err := yaml.Unmarshal(configData, &config); err != nil {
+    return err
+  }
+
+  fmt.Println(config.Key)
+
   return nil
 }
